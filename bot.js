ddc27477f62124e34ce8c0788889f2ad396b6c6d
yousefbots <45495875+yousefbots@users.noreply.github.com>
2018-12-25 19:59:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -349,7 +349,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`1play |  !M help`,"http://twitch.tv/MHELP")
+client.user.setGame(`!M help |  !M help`,"http://twitch.tv/MHELP")
 client.user.setStatus("dnd")
 });
 
