2ad8df032c8ff5f2f19dc5036c3dfe6a7e1e33ed
yousefbots <45495875+yousefbots@users.noreply.github.com>
2018-12-25 19:56:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ const gif = require("gif-search");
 
 const client = new Discord.Client({disableEveryone: true});
 
-const prefix = "2";
+const prefix = "!M";
 /////////////////////////
 ////////////////////////
 
@@ -349,7 +349,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`1play | Last Music`,"http://twitch.tv/Death Shop")
+client.user.setGame(`1play |  !M help`,"http://twitch.tv/MHELP")
 client.user.setStatus("dnd")
 });
 
